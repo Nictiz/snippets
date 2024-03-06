2944d82e4e580068a9c0a85398962f980ebf499a
Pieter Edelman <edelman@nictiz.nl>
2024-03-06 09:07:17 +0100

    Jira Cloud doesn't support the table format, so switch the output of Touchstone launcher to a list

--- TouchstoneCL/launcher.py
+++ TouchstoneCL/launcher.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser.add_argument("--production", action = "store_true")
     parser.add_argument("-T", help = f"Date T to use (default is '{touchstone.date_T}')")
     parser.add_argument("--start-only", action = "store_true", help = "Just launch the executions, don't wait for them to finish and don't report the results, unless it's explicitly defined that an execution should finish before continuing")
-    parser.add_argument("--jira-table", action = "store_true", help = "Print a summary in Jira table format after completion (ignored if --start-only is provided)")
+    parser.add_argument("--jira-table", action = "store_true", help = "Print a summary in Jira ;ost format after completion (ignored if --start-only is provided)")
     parser.add_argument("target", nargs = "*", help = "The targets to execute (both numbers and mnemonics are supported)")
     args = parser.parse_args()
 
@@ -34,21 +34,24 @@ if __name__ == "__main__":
         touchstone.executeTargets(targets, args.start_only)
 
         if not args.start_only and args.jira_table:
-            print("### Jira table ###")
-            print("||Target||Result||BITS||Opmerkingen||Execution")
+            print("### Jira list ###")
             for execution in touchstone.executions:
-                line =  f"|{execution.target.rel_path}|"
+                line = "* "
                 if execution.fails > 0:
-                    line += "(x)"
+                    line += "❌"
                 elif execution.warns > 0:
-                    line += "(?)"
+                    line += "⚠️"
                 else:
-                    line += "(/)"
-                if execution.fails > 0:
-                    line += f"\n{execution.fails} x failures"
-                if execution.warns > 0:
-                    line += f"\n{execution.warns} x warning"
-                line += f"| | |[https://touchstone.aegis.net/touchstone/execution?exec={execution.execution_id}]|"
+                    line += "✅"
+                line += f" [{execution.target.rel_path}](https://touchstone.aegis.net/touchstone/execution?exec={execution.execution_id})"
+                if execution.fails > 0 or execution.warns > 0:
+                    line += ": "
+                    if execution.fails > 0:
+                        line += f"{execution.fails} x failures"
+                        if execution.warns > 0:
+                            line += ", "
+                    if execution.warns > 0:
+                        line += f"\n{execution.warns} x warning"
                 print(line)
 
     finally:
